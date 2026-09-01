23244ba18bec9815ac3208274eefe9107c540ea4
Tae-beom <t224012@duksoo.hs.kr>
2026-09-01 11:56:05 +0000

    add_day3

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,6 +68,7 @@ st.markdown("""
 # --- 📌 단어장 파일 목록 설정 ---
 voca_files = {
     "Day 2 (해커스 보카)": "vocafile/Hackers_Voca_Day2_with_Synonyms.xlsx",
+    "Day 3 (해커스 보카)": "vocafile/Hackers_Voca_Day3_with_Synonyms.xlsx"
     # 나중에 추가할 파일명은 여기에 적어주세요.
 }
 
