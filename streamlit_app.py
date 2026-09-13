1008b1e6a3dca1c78cc840d4b64af4a0cc8ff11d
Tae-beom <t224012@duksoo.hs.kr>
2026-09-13 04:44:55 +0000

    day4-1

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,8 +69,8 @@ st.markdown("""
 voca_files = {
     "Day 2 (해커스 보카)": "vocafile/Hackers_Voca_Day2_with_Synonyms.xlsx",
     "Day 3 (해커스 보카)": "vocafile/Hackers_Voca_Day3_with_Synonyms.xlsx"
-    "Day 4 (해커스 보카)": "vocafile/Hackers_Voca_Day4_with_Synonyms.xlsx"
     "Day 4A (해커스 보카)": "vocafile/Hackers_Voca_Day4_1-23.xlsx",
+    "Day 4 (해커스 보카)": "vocafile/Hackers_Voca_Day4_with_Synonyms.xlsx"
     # 나중에 추가할 파일명은 여기에 적어주세요.
 }
 
